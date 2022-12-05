9e4554f321d387e01a29057735ba16dc4773b252
Jiqiang WANG_Robert <110020364+WANGJ203@users.noreply.github.com>
2022-12-06 00:17:51 +1300

    use UseEffect to check has token state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import {Routes, Route} from 'react-router-dom';
 import Home from "./components/home";
 import Category from "./components/category";
 import NavBar from "./components/NavBar";
+import Login from "./components/login";
 
 
 function App() {
-  return (
-    <div className="App">
-        <NavBar/>
-     <Routes>
-        <Route path="home" element={ <Home/> } />
-        <Route path="category" element={ <Category/> } />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+            <Routes>
+                <Route path="home" element={<Home/>}/>
+                <Route path="category" element={<Category/>}/>
+                <Route path="login" element={<Login/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import axios from "axios";
 import {BaseUrl} from "./constants";
 
@@ -8,6 +8,13 @@ function Login(props) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [hasToken, setHasToken] = useState(false)
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            setHasToken(true)
+            setToken(localStorage.getItem("token"))
+        }
+    },[token]);
 
     function usernameHandler(event) {
         setUsername(event.target.value)
@@ -28,7 +35,10 @@ function Login(props) {
                 headers: {},
             }
         ).then(response =>{
-            console.log(response)
+            console.log(response.data);
+            setToken(response.data)
+            setHasToken(true)
+            localStorage.setItem('token', response.data.token)
         }).catch(error => {
             console.log(error)
         });
@@ -37,14 +47,21 @@ function Login(props) {
 
     return (
         <div>
-            <Fragment>
+            {/*{hasToken?... : ...}*/}
+            {/*if has token, means already login, then appear the logout button, else show the login form*/}
+            {hasToken?
+                <Fragment>
+                    <button className={"btn btn-primary"}>Logout</button>
+                </Fragment>
+                :
+                <Fragment>
                 <p>Username:<input className={"form-control"} name={"username"} onChange={usernameHandler}/></p>
                 <p>Password:<input className={"form-control"} name={"password"} onChange={passwordHandler}/></p>
                 <p>
                     <button onClick={login}>Login</button>
                 </p>
-            </Fragment>
-
+                </Fragment>
+            }
         </div>
     );
 }
