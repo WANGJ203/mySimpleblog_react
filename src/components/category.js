a1d340f4346e9cf23e754b7bf5defb99d23cd96e
Jiqiang WANG_Robert <110020364+WANGJ203@users.noreply.github.com>
2022-12-05 16:49:28 +1300

    preparation for login

---
+++ src/components/category.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Category(props) {
+    return (
+        <div>
+            this is category page
+        </div>
+    );
+}
+
+export default Category;
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            this is homepage
+        </div>
+    );
+}
+
+export default Home;
